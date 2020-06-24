98cb27b38ed9d6005d1d04297272156210f80e42
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-06-24 21:26:37 +0530

    added security to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ export default function footer() {
             <footer className="footer">
                 <div className="content has-text-centered">
                     <p>
-                        Source code <a href='https://github.com/killcodeX/currconverter' target='_blank'><i className="fas fa-code-branch"></i></a>
+                        Source code <a href='https://github.com/killcodeX/currconverter' target='_blank' rel="noopener noreferrer"><i className="fas fa-code-branch"></i></a>
                     </p>
                 </div>
             </footer>
